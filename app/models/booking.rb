30a7c24bbd328baace2118b626d75ea484169fdd
Youssef Iraqui <youssef.iraqui@gmail.com>
2017-09-12 11:26:10 +0200

    validations ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :apartment
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
+  validates :status, presence: true
+
 end
