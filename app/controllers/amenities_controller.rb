68f1962a9f9d2476dcd683e275dd9bb457fa9a2b
Hendrik Wolleswinkel <hendrikwolleswinkel@me.com>
2017-09-12 16:57:25 +0200

    -allow-empty-message

--- app/controllers/amenities_controller.rb
+++ app/controllers/amenities_controller.rb
@@ -1,22 +1,10 @@
 class AmenitiesController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def new
+
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @amenity = Amenity.find(params[:id])
   end
 end
