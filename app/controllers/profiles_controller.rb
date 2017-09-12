89a905e594229c32f8b218b56998ce5383fb94ee
Youssef Iraqui <youssef.iraqui@gmail.com>
2017-09-12 15:25:51 +0200

    controllers profiles and reviews ok, controller user deleted

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,22 +1,51 @@
 class ProfilesController < ApplicationController
-  def index
-  end
+  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+
 
   def show
+    @profile = Profile.find(params[:id])
   end
 
   def new
+    @profile = Profile.new
+
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      redirect_to apartment_index_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+      if @profile.update(profile_params)
+        format.html { redirect_to @restaurant, notice: 'Profile was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
+    @profile.destroy
+    redirect_to apartment_index_path
   end
+
+
+  private
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :user_id)
+  end
+
 end
